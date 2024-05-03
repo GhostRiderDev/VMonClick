d632a874f87ee5c014ac8bac2d916d7e57b7fa1e
cos4h <olvadis2004@gmail.com>
2024-05-03 00:02:55 -0500

    first commit

---
+++ src/main/java/com/tds/VMonClick/VMonClick/VMonClickApplication.java
@@ -0,0 +1,13 @@
+package com.tds.VMonClick.VMonClick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VMonClickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VMonClickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tds/VMonClick/VMonClick/VMonClickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tds.VMonClick.VMonClick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VMonClickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
