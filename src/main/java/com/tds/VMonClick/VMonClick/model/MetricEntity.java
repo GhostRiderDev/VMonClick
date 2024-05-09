8579978bb048495c53d19e63c8c1b663d15a463f
cos4h <olvadis2004@gmail.com>
2024-05-08 20:06:42 -0500

    setup entities

---
+++ src/main/java/com/tds/VMonClick/VMonClick/model/MetricEntity.java
@@ -0,0 +1,40 @@
+package com.tds.VMonClick.VMonClick.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(value = "Metric")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MetricEntity {
+  @PrimaryKey
+  private UUID id;
+
+  @CassandraType(type = CassandraType.Name.INT)
+  private Integer cpu;
+
+  @CassandraType(type = CassandraType.Name.INT)
+  private Integer ram;
+
+  @CassandraType(type = CassandraType.Name.INT)
+  private Integer disk;
+
+  @CassandraType(type = CassandraType.Name.INT)
+  @Column("band_width")
+  private Integer bandWidth;
+
+  @Column("date_registered")
+  private LocalDateTime dateRegistered;
+
+  
+}
