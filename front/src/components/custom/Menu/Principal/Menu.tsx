6525022e8f5e481eebdc1524e629ab93c073a4fe
KeniBeck <deyler_mena69222@elpoli.edu.co>
2024-06-08 00:15:10 -0500

    modulacion de componentes para fronted

--- front/src/components/custom/Menu/Principal/Menu.tsx
+++ front/src/components/custom/Menu/Principal/Menu.tsx
@@ -5,56 +5,61 @@ import { SiBaremetrics } from "react-icons/si";
 import { GrVirtualMachine } from "react-icons/gr";
 import { FaHandsHelping } from "react-icons/fa";
 import { useState } from "react";
+import Footer from "@/Footer";
 
 function Menu() {
-  const [currentPage, setCurrentPage] = useState("Inicio");
-  return (
-    <div className=" bg-slate-100 flex w-screen">
-      <div className=" flex flex-col top-0 left-0 right-0 z-50 w-full">
-        <MenuPrincipal />
-        <div className=" w-full flex flex-row relative">
-          <div className="w-[300px]  sm:block hidden bg-[#0a0a0ae5] lg:h-[683px] lg:py-8 items-center sticky top-0 md:flex justify-center px-2">
-            <div className="w-full ">
-              <button
-                onClick={() => setCurrentPage("Inicio")}
-                className={`w-full h-[40px] flex items-center gap-6 text-white pl-2 ${
-                  currentPage === "Inicio"
-                    ? "bg-sky-400 text-black rounded-md"
-                    : "hover:bg-sky-400 hover:text-black hover:rounded-md"
-                }`}
-              >
-                <RiHomeOfficeFill className="mr-2 " />
-                Inicio
-              </button>
-              <button className="w-full h-[40px] flex items-center gap-6 pl-2 text-white hover:bg-sky-400 hover:text-black hover:rounded-md">
-                <SiBaremetrics className="mr-2" />
-                Metricas
-              </button>
-              <button className="w-full h-[40px] flex items-center gap-6 pl-2 text-white hover:bg-sky-400 hover:text-black hover:rounded-md">
-                <GrVirtualMachine className="mr-2" />
-                Maquinas
-              </button>
-              <button className="w-full h-[40px] flex items-center gap-6 pl-2 text-white hover:bg-sky-400 hover:text-black hover:rounded-md">
-                <FaHandsHelping className="mr-2" />
-                Ayuda
-              </button>
+    const [currentPage, setCurrentPage] = useState("Inicio");
+    return (
+        <div className=" bg-slate-900  w-full">
+            <div className=" flex flex-col top-0 left-0 right-0 z-50 w-full">
+                <div className="sticky top-0 z-50"><MenuPrincipal /></div>
+
+                <div className=" w-full flex flex-row">
+                    <div className="w-[300px]  sm:block hidden bg-slate-900 lg:h-[683px] lg:py-8 pt-2 sticky top-12 md:flex justify-center px-2">
+                        <div className="w-full ">
+                            <button
+                                onClick={() => setCurrentPage("Inicio")}
+                                className={`w-full h-[40px] flex items-center gap-6 text-white pl-2 ${currentPage === "Inicio"
+                                    ? "bg-sky-500 lg:text-black  rounded-md"
+                                    : "hover:bg-sky-400 hover:text-black hover:rounded-md"
+                                    }`}
+                            >
+                                <RiHomeOfficeFill className="mr-2 " />
+                                Inicio
+                            </button>
+                            <button className="w-full h-[40px] flex items-center gap-6 pl-2 text-white hover:bg-sky-500 hover:text-black hover:rounded-md">
+                                <SiBaremetrics className="mr-2" />
+                                Metricas
+                            </button>
+                            <button className="w-full h-[40px] flex items-center gap-6 pl-2 text-white hover:bg-sky-500 hover:text-black hover:rounded-md">
+                                <GrVirtualMachine className="mr-2" />
+                                Maquinas
+                            </button>
+                            <button className="w-full h-[40px] flex items-center gap-6 pl-2 text-white hover:bg-sky-500 hover:text-black hover:rounded-md">
+                                <FaHandsHelping className="mr-2" />
+                                Ayuda
+                            </button>
+                        </div>
+                    </div>
+                    <div className="w-full bg-slate-950 px-8 pb-4 space-y-6 lg:py-8 h-max rounded-md ">
+                        <div className="w-full text-center space-y-2 lg:text-left lg:space-y-10 lg:pl-[18px]">
+                            <h1 className="text-3xl pl-2 pt-4 ">
+                                <strong className="text-white">Bienvenido</strong>
+                            </h1>
+                            <p className="text-2xl text-white">
+                                Gestiona tus maquinas virtuales de una manera mas sencilla,comoda
+                                y segura.
+                            </p>
+                        </div>
+
+                        <section>
+                            <MetricsTesting />
+                        </section>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="w-full bg-[#0a0a0ae5] px-8 pb-4 space-y-6 lg:py-8 h-max">
-            <h1 className="text-3xl pl-2 pt-4 ">
-              <strong className="text-white">Bienvenido</strong>
-            </h1>
-            <p className="text-2xl text-white">
-              Gestiona tus maquinas virtuales de una manera mas sencilla,comoda
-              y segura.
-            </p>
-            <section>
-              <MetricsTesting />
-            </section>
-          </div>
+            <Footer />
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 export default Menu;
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,13 +4,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import Footer from './Footer.tsx'
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Footer />
     </BrowserRouter>
   </React.StrictMode>,
 )
