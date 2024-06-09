4b6d5e11892a1fbeb7d90eb4eb04fc4412e45e8d
KeniBeck <deyler_mena69222@elpoli.edu.co>
2024-06-09 01:29:20 -0500

    protecion de rutas

--- front/src/components/custom/login/LoginFrom.tsx
+++ front/src/components/custom/login/LoginFrom.tsx
@@ -1,11 +1,27 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+import { AlertError, AlertSuccess } from "@/components/Alerts/Alerts";
 type Inputs = {
-    user: string,
-    pass: string,
+    email: string,
+    password: string,
 };
 function LoginFrom() {
     const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+    const onSubmit: SubmitHandler<Inputs> = data => {
+        axios.post('http://localhost:8080/users/login', data)
+            .then(response => {
+                console.log(response.data);
+                AlertSuccess("", "Bienvenido");
+                sessionStorage.setItem('token', response.data.token);
+                sessionStorage.setItem('user', JSON.stringify(response.data.user));
+                window.location.href = '/menu';
+            })
+            .catch(error => {
+                console.log(error);
+                AlertError("", "Usuario o contraseña incorrecta");
+            })
+
+    }
 
     return (
         <div className="w-full h-screen  flex justify-center items-center bg-[url(/datacenter.jpg)] bg-cover bg-center" >
@@ -18,14 +34,14 @@ function LoginFrom() {
                     <form onSubmit={handleSubmit(onSubmit)} className="lg:w-[380px]  lg:rounded-md px-8 space-y-4 pt-2 pb-4 bg-[#050505d3]">
                         <h1 className="text-center text-lg text-white"><strong>Login</strong></h1>
                         <div className="w-full">
-                            <label className="text-white">Usuario</label>
-                            <input  {...register("user", { required: true })} className="w-full border border-slate-600 rounded-md p-1 pl-2" />
-                            {errors.user && <span className="text-red-500">Campo requerido</span>}
+                            <label className="text-white">Correo</label>
+                            <input  {...register("email", { required: true })} className="w-full border border-slate-600 rounded-md p-1 pl-2" />
+                            {errors.email && <span className="text-red-500">Campo requerido</span>}
                         </div>
                         <div className="w-full">
                             <label className="text-white">Contraseña</label>
-                            <input {...register("pass", { required: true })} className="w-full border border-slate-600 rounded-md p-1 pl-2" type="password" />
-                            {errors.pass && <span className="text-red-500">Campo requerido</span>}
+                            <input {...register("password", { required: true })} className="w-full border border-slate-600 rounded-md p-1 pl-2" type="password" />
+                            {errors.password && <span className="text-red-500">Campo requerido</span>}
                         </div>
                         <div className="w-full flex flex-col justify-center items-center space-y-3  ">
                             <button type="submit" className="w-full bg-blue-500 text-white h-[35px] rounded-md">Ingresar</button>
